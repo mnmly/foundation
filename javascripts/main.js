c2c83a045ff4162c09b0f873b245a711249aae70
mnmly <i.am@mnmly.com>
2012-03-29 20:06:36 -0500

    Added underscore and backbone too

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,17 +3,14 @@
   require.config({
     baseUrl: '/javascripts',
     paths: {
-      order: "order",
-      jquery: "jquery.min",
-      modernizr: "modernizr.foundation",
-      reveal: "jquery.reveal",
-      orbit: "jquery.orbit-1.4.0",
-      customforms: "jquery.customforms",
-      placeholder: "jquery.placeholder.min",
-      tooltips: "jquery.tooltips",
-      app: "app"
+      jquery: 'libs/jquery.min',
+      backbone: 'libs/backbone-min',
+      underscore: 'libs/underscore-min',
+      order: 'libs/rjs-plugins/order',
+      text: 'libs/rjs-plugins/text',
+      json: 'libs/rjs-plugins/json'
     },
-    priority: ['modernizr', 'jquery']
+    priority: ['underscore', 'jquery', 'backbone', 'text', 'order']
   });
 
   require(['reveal', 'orbit', 'customforms', 'placeholder', 'tooltips', 'app'], function() {
