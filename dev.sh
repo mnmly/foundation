265965902832f797c631f00e3d7e62484071b805
mnmly <i.am@mnmly.com>
2012-04-22 00:19:34 -0500

    Added nib

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
-coffee -wc -o javascripts src/coffee/*.coffee & \
-./node_modules/stylus/bin/stylus -w src/stylus/*.styl -o stylesheets/ & \
+coffee -wc -o javascripts src/coffee & \
+./node_modules/stylus/bin/stylus -w src/stylus -o stylesheets/ --use ./node_modules/nib/lib/nib & \
 ./node_modules/livereload/bin/livereload & \
 serve
